cb0ac93a793c96b3cf96cf69d4bc72bdf9d4611a
Daniel <kingdread@gmx.de>
2015-05-19 21:57:09 +0200

    Add more turtle methods
    - teleport
    - set_orientation
    - home
    - get_{orientation, position}

--- src/turtle.rs
+++ src/turtle.rs
@@ -76,9 +76,8 @@ impl Turtle {
     /// negative means clockwise. The angle is given in degrees. This function
     /// is used internally.
     fn turn(&mut self, deg: f32) {
-        self.orientation = (self.orientation + deg) % 360.0;
-        self.screen.turtle_orientation = self.orientation;
-        self.screen.draw_and_update();
+        let orientation = self.orientation;
+        self.set_orientation(orientation + deg);
     }
 
     /// Take the length of a path and return the (delta_x, delta_y) attributes
@@ -132,4 +131,31 @@ impl Turtle {
         self.color = (red, green, blue, 1.0);
         self.screen.turtle_color = self.color;
     }
+
+    /// Directly move the turtle to the given point without changing the
+    /// direction. Draws a line if the pen is down. Note that the origin (0, 0)
+    /// is in the center of the screen with positive coordinates being right/top
+    /// and negative ones left/down.
+    pub fn teleport(&mut self, x: f32, y: f32) {
+        self.goto(x, y)
+    }
+
+    /// Set the turtle's orientation in degrees with 0 being faced north and
+    /// positive degrees counting counter-clockwise.
+    pub fn set_orientation(&mut self, deg: f32) {
+        self.orientation = deg % 360.0;
+        self.screen.turtle_orientation = self.orientation;
+        self.screen.draw_and_update();
+    }
+
+    /// Move the turtle to the origin and set its orientation to 0
+    pub fn home(&mut self) {
+        self.teleport(0.0, 0.0);
+        self.set_orientation(0.0);
+    }
+
+    /// Return the turtle's orientation
+    pub fn get_orientation(&self) -> f32 { self.orientation }
+    /// Return the turtle's position
+    pub fn get_position(&self) -> (f32, f32) { self.position }
 }
