5d5c58f7c3a4cc18754c8d6c34f785774db4fc4e
Daniel <kingdread@gmx.de>
2015-05-21 16:27:59 +0200

    Add parser code

---
+++ src/parse/ast.rs
@@ -0,0 +1,97 @@
+#[derive(Debug, Clone)]
+pub enum Node {
+    /// A list of statements as found inside a loop body
+    StatementList(Vec<Box<Node>>),
+    /// The if conditional (expression, true-clause, maybe false-clause)
+    IfStatement(Box<Node>, Box<Node>, Option<Box<Node>>),
+    /// The repeat statement (count, loop body)
+    RepeatStatement(Box<Node>, Box<Node>),
+    /// The while statement (condition, loop body)
+    WhileStatement(Box<Node>, Box<Node>),
+    /// The function definition statement (func name, func arg names, func body)
+    LearnStatement(String, Vec<String>, Box<Node>),
+    Comparison(Box<Node>, CompOp, Box<Node>),
+    /// Addition or subtraction. One addition may hold more than one operation.
+    Addition(Box<Node>, Vec<(AddOp, Box<Node>)>),
+    /// Multiplication and division. One multiplication may hole more than one
+    /// operation.
+    Multiplication(Box<Node>, Vec<(MulOp, Box<Node>)>),
+    /// A function call (function, arguments)
+    FuncCall(String, Vec<Box<Node>>),
+    List(Vec<Box<Node>>),
+    StringLiteral(String),
+    Number(f32),
+    Variable(String),
+}
+
+/// Helper function to flatten a vector of boxes to nodes
+fn flatten(mut input: Vec<Box<Node>>) -> Vec<Box<Node>> {
+    let mut result = Vec::new();
+    while !input.is_empty() {
+        result.push(Box::new(input.remove(0).flatten()));
+    }
+    result
+}
+
+fn flatten_tuple<T>(mut input: Vec<(T, Box<Node>)>) -> Vec<(T, Box<Node>)> {
+    let mut result = Vec::new();
+    while !input.is_empty() {
+        let (op, elem) = input.remove(0);
+        result.push((op, Box::new(elem.flatten())));
+    }
+    result
+}
+
+impl Node {
+    /// Consume the node and produce a flat version
+    pub fn flatten(self) -> Node {
+        use self::Node::*;
+        match self {
+            Addition(sum, summands) => {
+                if summands.is_empty() {
+                    sum.flatten()
+                } else {
+                    Addition(Box::new(sum.flatten()), flatten_tuple(summands))
+                }
+            },
+            Multiplication(mul, factors) => {
+                if factors.is_empty() {
+                    mul.flatten()
+                } else {
+                    Multiplication(Box::new(mul.flatten()), flatten_tuple(factors))
+                }
+            }
+            StatementList(stmts) => StatementList(flatten(stmts)),
+            IfStatement(cond, true_body, false_body) => {
+                if let Some(stmt) = false_body {
+                    IfStatement(Box::new(cond.flatten()), Box::new(true_body.flatten()),
+                                Some(Box::new(stmt.flatten())))
+                } else {
+                    IfStatement(Box::new(cond.flatten()), Box::new(true_body.flatten()), None)
+                }
+            },
+            RepeatStatement(count, body) => RepeatStatement(Box::new(count.flatten()),
+                                                            Box::new(body.flatten())),
+            WhileStatement(cond, body) => WhileStatement(Box::new(cond.flatten()),
+                                                         Box::new(body.flatten())),
+            LearnStatement(name, args, body) => LearnStatement(name, args,
+                                                               Box::new(body.flatten())),
+            Comparison(operand1, op, operand2) => Comparison(Box::new(operand1.flatten()),
+                                                             op,
+                                                             Box::new(operand2.flatten())),
+            FuncCall(name, args) => FuncCall(name, flatten(args)),
+            node => node,
+        }
+    }
+}
+
+/// Different comparison operators
+#[derive(Debug, Copy, Clone)]
+pub enum CompOp {
+    Equal, Less, Greater, LessEqual, GreaterEqual, NotEqual,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum AddOp { Add, Sub }
+#[derive(Debug, Copy, Clone)]
+pub enum MulOp { Mul, Div }
---
+++ src/parse/mod.rs
@@ -0,0 +1,333 @@
+//! Parsing module for Rurtle programs.
+//!
+//! Note that parsing requires some additional information, i.e. the number of
+//! arguments for a function. Function calls in Rurtle need neither parenthesis
+//! nor something else, so this is legal:
+//!
+//! ```text
+//! FUNCA FUNCB 10
+//! ```
+//!
+//! Depending on how many arguments each function takes, this may be parsed as
+//! either `funca(funcb(10))` or `funca(funcb(), 10)`.
+//!
+//! # Grammar
+//!
+//! A EBNF-like (incomplete) grammar may look like
+//!
+//! ```text
+//! root := {statement} ;
+//! statement := learn-def | if-stmt | repeat-stmt | while-stmt | expression ;
+//! learn-def := 'LEARN' identifier {variable} 'DO' {statement} 'END' ;
+//! if-stmt := 'IF' expression 'DO' {statement} ['ELSE' {statement}]'END' ;
+//! repeat-stmt := 'REPEAT' expression 'DO' {statement} 'END' ;
+//! while-stmt := 'WHILE' expression 'DO' {statement} 'END' ;
+//! variable := ':' identifier ;
+//! identifier := idenfitier-start {identifier-cont} ;
+//! idenfitier-start := <any alphabetic character> ;
+//! idenfitier-cont := <any alpabetic or numeric character> ;
+//! expression := comparison ;
+//! comparison := expr [comp_op expr] ;
+//! comp_op := '=' | '<' | '>' | ''<=' | '>=' | '<>' ;
+//! expr := product {('+' | '-') product} ;
+//! product := factor {('*' | '/') factor} ;
+//! factor := '(' expression ')' | list | variable | string | number | (identifier {expression}) ;
+//! list := '[' {expression} ']' ;
+//! string := '"' {<any character>} '"' ;
+//! number := <any valid floating point number literal> ;
+//! ```
+pub mod ast;
+
+use super::lex::Token;
+use self::ast::{Node, AddOp, MulOp, CompOp};
+use self::ast::Node::*;
+use std::collections::HashMap;
+
+/// A `FuncMap` maps the name of a function to the number of arguments it takes
+pub type FuncMap = HashMap<String, i32>;
+
+/// A `Parser` builds an AST from the given input token stream.
+pub struct Parser {
+    tokens: Vec<Token>,
+    functions: FuncMap,
+}
+
+#[derive(Debug)]
+pub enum ParseError {
+    UnexpectedToken(Token),
+    UnexpectedEnd,
+    UnknownFunction(String),
+}
+
+impl ::std::fmt::Display for ParseError {
+    fn fmt(&self, fmt: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
+        use self::ParseError::*;
+        match self {
+            &UnexpectedToken(ref what) => {
+                try!(fmt.pad("unexpected token: "));
+                what.fmt(fmt)
+            },
+            &UnexpectedEnd => fmt.pad("unexpected end"),
+            &UnknownFunction(ref name) => {
+                try!(fmt.pad("unknown function: "));
+                name.fmt(fmt)
+            }
+        }
+    }
+}
+
+impl ::std::error::Error for ParseError {
+    fn description(&self) -> &str {
+        use self::ParseError::*;
+        match self {
+            &UnexpectedToken(_) => "unexpected token",
+            &UnexpectedEnd => "unexpected end of input",
+            &UnknownFunction(_) => "the parser doesn't know the function",
+        }
+    }
+}
+
+pub type ParseResult = Result<Node, ParseError>;
+
+macro_rules! expect {
+    ($s:expr, $t:pat) => {
+        {
+            if $s.tokens.is_empty() {
+                return Err(ParseError::UnexpectedEnd);
+            };
+            let token = $s.tokens[0].clone();
+            match token {
+                $t => $s.tokens.remove(0),
+                _ => return Err(ParseError::UnexpectedToken(token)),
+            }
+        }
+    }
+}
+
+macro_rules! pop_left {
+    ($s:expr) => {
+        {
+            if $s.tokens.is_empty() {
+                return Err(ParseError::UnexpectedEnd)
+            } else {
+                $s.pop_left()
+            }
+        }
+    }
+}
+
+impl Parser {
+    /// Construct a new `Parser`, consuming the given tokens.
+    pub fn new(tokens: Vec<Token>, functions: FuncMap) -> Parser {
+        Parser {
+            tokens: tokens,
+            functions: functions,
+        }
+    }
+
+    /// Attempt to return the root node
+    pub fn parse(&mut self) -> ParseResult {
+        self.parse_statement_list()
+    }
+
+    fn peek(&self) -> Token {
+        self.tokens[0].clone()
+    }
+
+    fn pop_left(&mut self) -> Token {
+        self.tokens.remove(0)
+    }
+
+    fn parse_statement_list(&mut self) -> ParseResult {
+        let mut statements = Vec::new();
+        while !self.tokens.is_empty() {
+            let statement = try!(self.parse_statement());
+            statements.push(Box::new(statement));
+        }
+        Ok(StatementList(statements))
+    }
+
+    fn parse_loop_body(&mut self) -> ParseResult {
+        let mut statements = Vec::new();
+        while !self.tokens.is_empty() {
+            match self.peek() {
+                Token::KeyElse | Token::KeyEnd => break,
+                _ => {
+                    statements.push(Box::new(try!(self.parse_statement())));
+                },
+            }
+        }
+        Ok(StatementList(statements))
+    }
+
+    fn parse_statement(&mut self) -> ParseResult {
+        let token = self.peek();
+        match token {
+            Token::KeyLearn => self.parse_learn_stmt(),
+            Token::KeyIf => self.parse_if_stmt(),
+            Token::KeyRepeat => self.parse_repeat_stmt(),
+            _ => self.parse_expression(),
+        }
+    }
+
+    fn parse_learn_stmt(&mut self) -> ParseResult {
+        expect!(self, Token::KeyLearn);
+        let name = match pop_left!(self) {
+            Token::Word(string) => string,
+            token => return Err(ParseError::UnexpectedToken(token)),
+        };
+        let mut variables = Vec::new();
+        while !self.tokens.is_empty() {
+            match pop_left!(self) {
+                Token::Colon => {
+                    match pop_left!(self) {
+                        Token::Word(s) => variables.push(s),
+                        token => return Err(ParseError::UnexpectedToken(token)),
+                    }
+                },
+                Token::KeyDo => break,
+                token => return Err(ParseError::UnexpectedToken(token)),
+            }
+        }
+        // We need the argument count for this function if it appears later
+        // during the parsing stage (e.g. in a recursive call)
+        self.functions.insert(name.clone(), variables.len() as i32);
+        let statements = try!(self.parse_loop_body());
+        expect!(self, Token::KeyEnd);
+        Ok(LearnStatement(name, variables, Box::new(statements)))
+    }
+
+    fn parse_if_stmt(&mut self) -> ParseResult {
+        expect!(self, Token::KeyIf);
+        let condition = Box::new(try!(self.parse_expression()));
+        expect!(self, Token::KeyDo);
+        let true_body = Box::new(try!(self.parse_loop_body()));
+        let false_body = if let Token::KeyElse = self.peek() {
+            self.pop_left();
+            Some(Box::new(try!(self.parse_loop_body())))
+        } else { None };
+        expect!(self, Token::KeyEnd);
+        Ok(IfStatement(condition, true_body, false_body))
+    }
+
+    fn parse_repeat_stmt(&mut self) -> ParseResult {
+        expect!(self, Token::KeyRepeat);
+        let number = Box::new(try!(self.parse_expression()));
+        expect!(self, Token::KeyDo);
+        let body = try!(self.parse_loop_body());
+        Ok(RepeatStatement(number, Box::new(body)))
+    }
+
+    fn parse_expression(&mut self) -> ParseResult {
+        self.parse_comparison()
+    }
+
+    fn parse_comparison(&mut self) -> ParseResult {
+        let operand = try!(self.parse_expr());
+        if self.tokens.is_empty() {
+            return Ok(operand);
+        };
+        match self.peek() {
+            Token::OpEq | Token::OpLt | Token::OpGt |
+            Token::OpLe | Token::OpGe | Token::OpNe => {
+                let op = match self.pop_left() {
+                    Token::OpEq => CompOp::Equal,
+                    Token::OpLt => CompOp::Less,
+                    Token::OpGt => CompOp::Greater,
+                    Token::OpLe => CompOp::LessEqual,
+                    Token::OpGe => CompOp::GreaterEqual,
+                    Token::OpNe => CompOp::NotEqual,
+                    _ => unreachable!(),
+                };
+                let operand_right = Box::new(try!(self.parse_expr()));
+                Ok(Comparison(Box::new(operand), op, operand_right))
+            }
+            _ => Ok(operand),
+        }
+    }
+
+    fn parse_expr(&mut self) -> ParseResult {
+        let product = Box::new(try!(self.parse_product()));
+        let mut addends = Vec::new();
+        while !self.tokens.is_empty() {
+            match self.peek() {
+                Token::OpPlus | Token::OpMinus => {
+                    let op = match self.pop_left() {
+                        Token::OpPlus => AddOp::Add,
+                        Token::OpMinus => AddOp::Sub,
+                        _ => unreachable!(),
+                    };
+                    addends.push((op, Box::new(try!(self.parse_product()))));
+                },
+                _ => break,
+            }
+        }
+        Ok(Addition(product, addends))
+    }
+
+    fn parse_product(&mut self) -> ParseResult {
+        let factor = Box::new(try!(self.parse_factor()));
+        let mut factors = Vec::new();
+        while !self.tokens.is_empty() {
+            match self.peek() {
+                Token::OpMul | Token::OpDiv => {
+                    let op = match self.pop_left() {
+                        Token::OpMul => MulOp::Mul,
+                        Token::OpDiv => MulOp::Div,
+                        _ => unreachable!(),
+                    };
+                    factors.push((op, Box::new(try!(self.parse_factor()))));
+                },
+                _ => break,
+            }
+        }
+        Ok(Multiplication(factor, factors))
+    }
+
+    fn parse_factor(&mut self) -> ParseResult {
+        if self.tokens.is_empty() {
+            return Err(ParseError::UnexpectedEnd);
+        };
+        match self.pop_left() {
+            Token::LParens => {
+                let factor = try!(self.parse_expression());
+                expect!(self, Token::RParens);
+                Ok(factor)
+            },
+            Token::LBracket => {
+                let mut list = Vec::new();
+                while !self.tokens.is_empty() {
+                    if let Token::RBracket = self.peek() {
+                        break
+                    }
+                    list.push(Box::new(try!(self.parse_expression())));
+                }
+                expect!(self, Token::RBracket);
+                Ok(List(list))
+            },
+            Token::Colon => {
+                if let Token::Word(name) = self.peek() {
+                    self.pop_left();
+                    Ok(Variable(name))
+                } else {
+                    Err(ParseError::UnexpectedToken(self.pop_left()))
+                }
+            },
+            // A function call
+            Token::Word(name) => {
+                let argument_count = match self.functions.get(&name) {
+                    Some(i) => *i,
+                    None => return Err(ParseError::UnknownFunction(name)),
+                };
+                let mut arguments = Vec::new();
+                for _ in (0..argument_count) {
+                    arguments.push(Box::new(try!(self.parse_expression())));
+                }
+                Ok(FuncCall(name, arguments))
+            },
+            Token::String(string) => Ok(StringLiteral(string)),
+            Token::Number(num) => Ok(Number(num)),
+            token => Err(ParseError::UnexpectedToken(token)),
+        }
+    }
+}
