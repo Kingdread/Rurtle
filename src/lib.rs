4a83fbf435e52fa756fdd7b4a151bae17c93aa57
Daniel <kingdread@gmx.de>
2015-05-24 18:27:51 +0200

    Add lib.rs to git

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+//! Rurtle
+#![feature(collections)]
+#[macro_use]
+extern crate glium;
+
+pub mod graphic;
+pub use graphic::TurtleScreen;
+pub use graphic::color;
+
+pub mod turtle;
+pub use turtle::Turtle;
+
+pub mod lex;
+pub use lex::tokenize;
+
+pub mod parse;
+pub use parse::Parser;
+
+pub mod environ;
+pub use environ::Environment;
