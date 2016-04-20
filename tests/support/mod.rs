bf39ded8661d5b1b251465789b28981c12c15525
Daniel Schadt <kingdread@gmx.de>
2016-04-20 16:23:18 +0200

    tests: add runtime tests
    These tests actually compare the resulting image.
    
    Test files should be placed under
        tests/runtime/*.rtl
    and the expected image with the same base name as
        tests/runtime/*.png

--- tests/support/mod.rs
+++ tests/support/mod.rs
@@ -1,4 +1,5 @@
 use rurtle;
+pub extern crate image;
 
 /// Dimensions used for the test image
 pub const DIMENSIONS: (u32, u32) = (800, 400);
@@ -9,4 +10,13 @@ pub fn screen() -> rurtle::TurtleScreen {
 
 pub fn turtle() -> rurtle::Turtle {
     rurtle::Turtle::new(screen())
+}
+
+pub fn environ() -> rurtle::Environment {
+    rurtle::Environment::new(turtle())
+}
+
+pub fn image_eq(a: &image::DynamicImage, b: &image::DynamicImage) -> bool {
+    use self::image::GenericImage;
+    a.pixels().eq(b.pixels())
 }
