5eed1555a068c01a171f9daf0eb722cbceaf668c
Daniel Schadt <kingdread@gmx.de>
2016-04-20 19:09:49 +0200

    Revert "tests: add runtime tests"
    This reverts commit bf39ded8661d5b1b251465789b28981c12c15525.
    
    The differences between different systems prevent a simple image
    comparison. As long as no better matching algorithm is implemented, the
    tests will be useless and thus removed.

--- tests/support/mod.rs
+++ tests/support/mod.rs
@@ -1,5 +1,4 @@
 use rurtle;
-pub extern crate image;
 
 /// Dimensions used for the test image
 pub const DIMENSIONS: (u32, u32) = (800, 400);
@@ -10,13 +9,4 @@ pub fn screen() -> rurtle::TurtleScreen {
 
 pub fn turtle() -> rurtle::Turtle {
     rurtle::Turtle::new(screen())
-}
-
-pub fn environ() -> rurtle::Environment {
-    rurtle::Environment::new(turtle())
-}
-
-pub fn image_eq(a: &image::DynamicImage, b: &image::DynamicImage) -> bool {
-    use self::image::GenericImage;
-    a.pixels().eq(b.pixels())
 }
